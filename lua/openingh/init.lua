ba9664ce7b0542b79b90971b897ed8bd65c30cbe
Ali Almohaya <alialmohaya@gmail.com>
2022-09-25 23:53:23 +0300

    chore: add todos to the main files

--- lua/openingh/init.lua
+++ lua/openingh/init.lua
@@ -1,1 +1,21 @@
-print("HELLOO FROM LUAS")
+
+local M = {
+  setup = function (debug)
+    -- TODO - 1: add the repo path to global var
+    if (debug) then
+      print("debugging openingh.nvim")
+    end
+  end,
+
+  openFile = function ()
+    -- TODO - 1: get the url from git folder
+    -- TODO - 2: get the current line in the buffer and add it to the file url 
+    -- TODO - 3: get the selected range in the buffer and add it to the file url 
+  end,
+
+  openRepo = function ()
+    -- TODO - 1: get the remote url and open it
+  end
+}
+
+return M
---
+++ lua/openingh/utils.lua
@@ -0,0 +1,8 @@
+local M = {
+  get_git_dir = function ()
+  end,
+  get_selection_numbers = function ()
+  end
+}
+
+return M
