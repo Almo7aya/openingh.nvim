8ea62df177878605817430b8404dbe5349ec9282
Ali Almohaya <alialmohaya@gmail.com>
2022-10-30 00:45:49 +0300

    feat: testing unit

--- tests/openingh_spec.lua
+++ tests/openingh_spec.lua
@@ -1,15 +1,18 @@
 local eq = assert.are.same
 local busted = require("plenary/busted")
 
+before_each(function()
+  vim.cmd([[packadd openingh.nvim]])
+end)
+
 describe("busted should run ", function()
   it(" should start test", function()
-    vim.cmd([[packadd openingh.nvim]])
     local status = require("plenary.reload").reload_module(".nvim")
     eq(status, nil)
   end)
 
   it("openingh should be available", function()
     local status = require("openingh")
-    eq(status, nil)
+    assert.truthy(status)
   end)
 end)
