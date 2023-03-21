0882b3d798edd64c7fc9611cfce5bb9e383944e4
Laurent Vauthrin <lvauthrin@gmail.com>
2023-03-21 09:39:12 -0400

    Added support for ranges when opening a file

--- plugin/openingh.lua
+++ plugin/openingh.lua
@@ -5,9 +5,15 @@ vim.g.openingh = true
 
 local openingh = require("openingh")
 
-vim.api.nvim_create_user_command("OpenInGHFile", function()
-  openingh:openFile()
-end, {})
+vim.api.nvim_create_user_command("OpenInGHFile", function(opts)
+  if opts.range == 0 or opts.range == 1 then
+    openingh.openFile()
+  else
+    openingh.openFile(opts.line1, opts.line2)
+  end
+end, {
+  range = true,
+})
 
 vim.api.nvim_create_user_command("OpenInGHRepo", function()
   openingh:openRepo()
