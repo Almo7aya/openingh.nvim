b9daa9db78c5a5586b637c0f309c5e9c502ed508
Ali Almohaya <alialmohaya@gmail.com>
2023-05-26 22:31:02 +0300

    feat: support OpenInGHFileLines command

--- plugin/openingh.lua
+++ plugin/openingh.lua
@@ -15,6 +15,16 @@ end, {
   range = true,
 })
 
+vim.api.nvim_create_user_command("OpenInGHFileLines", function(opts)
+  if opts.range == 0 then -- Nothing was selected
+    openingh.open_file(opts.line1)
+  else -- Current line or block was selected
+    openingh.open_file(opts.line1, opts.line2)
+  end
+end, {
+  range = true,
+})
+
 vim.api.nvim_create_user_command("OpenInGHRepo", function()
   openingh.open_repo()
 end, {})
